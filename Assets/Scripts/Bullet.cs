35b5807880c55a29a14225ae7a0afe1657dcbfb1
ata kaleli <atakaleli123@gmail.com>
2024-02-07 13:56:35 +0300

    ScoreUI Connected

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -17,12 +17,15 @@ public class Bullet : MonoBehaviour
     {
         transform.right = rb.velocity;
     }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Target")
         {
             Destroy(gameObject);
             Destroy(collision.gameObject);
+            UIManager.instance.AddScore();
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
 
+    public static UIManager instance; //make UI accessable from anywhere and anyplace
+
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI ammoText;
     [SerializeField] private TextMeshProUGUI timerText;
 
     private float timer = 0;
-
+    private int scoreValue = 0;
 
     void Start()
     {
-        
+        instance = this; // meaningful if we have only  1 UI as an instance
+
     }
 
     // Update is called once per frame
@@ -23,11 +26,24 @@ public class UIManager : MonoBehaviour
     {
         timer += Time.deltaTime;
         ShowTimer();
+
+       
+
+
     }
+
+
+
 
     private void ShowTimer()
     {
         if (timer >= 1)
             timerText.text = timer.ToString("#,#");
+    }
+
+    public void AddScore()
+    {
+        scoreValue++;
+        scoreText.text = scoreValue.ToString();
     }
 }
