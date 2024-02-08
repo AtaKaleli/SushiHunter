6be7452d17e762d6e1a823cfc4a63eddddf7c161
ata kaleli <atakaleli123@gmail.com>
2024-02-08 15:28:48 +0300

    powershot added

---
+++ Assets/Scripts/Spawners/PoweshotSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweshotSpawner : MonoBehaviour
+{
+    [SerializeField] private float specialShotCooldown = 8;
+    [SerializeField] private float flingForce = 500;
+    [SerializeField] private GameObject bulletPref;
+    private float timer;
+
+
+    private void Awake()
+    {
+        timer = specialShotCooldown;
+    }
+
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.F)) //&& timer >= specialShotCooldown)
+        {
+            
+            
+
+            for (int i = 0; i < 18; i++)
+            {
+                float xCoordinate = i - 8.5f;
+                GameObject newShoot = Instantiate(bulletPref, new Vector3(xCoordinate, -6, 0), Quaternion.identity);
+                Rigidbody2D shootRB = newShoot.GetComponent<Rigidbody2D>();
+                shootRB.AddForce(new Vector2(0, flingForce)); // Adjust the force or any other parameters as needed
+                Destroy(newShoot, 5);
+            }
+            timer = 0;
+            
+        }
+
+    }
+
+
+}
